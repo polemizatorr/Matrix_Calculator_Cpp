e65f44e46887a2e1e79e598647f4cf0ea6bf4736
polemizatorr <dodo120@vp.pl>
2019-04-01 13:17:08 +0200

    Create Interface.h

---
+++ Interface.h
@@ -0,0 +1,17 @@
+#ifndef INTERFACE_H_INCLUDED
+#define INTERFACE_H_INCLUDED
+#include "Macierz.h"
+#include "Macierz.cpp"
+using namespace std;
+
+void menu();
+void fun_addition();
+void fun_subtraction();
+void fun_multiplicationC();
+void fun_multiplicationM();
+void Transposite();
+
+
+
+
+#endif // INTERFACE_H_INCLUDED
